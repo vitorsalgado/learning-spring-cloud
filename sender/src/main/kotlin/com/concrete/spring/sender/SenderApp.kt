df4c60112e9d0d626e4efb9e238de4db151505f3
vitorsalgado <vsalgadopb@gmail.com>
2018-07-24 20:49:39 -0300

    service discovery

--- sender/src/main/kotlin/com/concrete/spring/sender/SenderApp.kt
+++ sender/src/main/kotlin/com/concrete/spring/sender/SenderApp.kt
@@ -2,6 +2,7 @@ package com.concrete.spring.sender
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
